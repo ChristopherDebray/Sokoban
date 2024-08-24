441531366d9298b9129b2cc7423cfdb24fad24b8
Christopher Debray <christopherdebray1@gmail.com>
2024-08-24 17:30:00 +0200

    feat: Add basic tile data setup for layer tileLayers

---
+++ autoloads/level_data.gd
@@ -0,0 +1,39 @@
+extends Node
+
+const LEVEL_DATA_PATH: String = "res://data/level_data.json"
+const TILE_SIZE: int = 32
+
+var _level_data: Dictionary = {} 
+
+func _ready() -> void:
+	load_level_data()
+	pass # Replace with function body.
+
+func add_tiles_for_layer(
+	layout: LevelLayout,
+	layer_type: TileLayers.LayerType,
+	tile_coords: Array
+) -> void:
+	for tile_coord in tile_coords:
+		layout.add_tile_to_layer(tile_coord.x, tile_coord.y, layer_type)
+
+func setup_level(lns: String, raw_lvl_data: Dictionary) -> LevelLayout:
+	var layout: LevelLayout = LevelLayout.new()
+	var raw_tiles: Dictionary = raw_lvl_data.tiles
+	var ps: Dictionary = raw_lvl_data['player_start']
+	
+	add_tiles_for_layer(layout, TileLayers.LayerType.FLOOR, raw_tiles.Floor)
+	add_tiles_for_layer(layout, TileLayers.LayerType.WALL, raw_tiles.Walls)
+	add_tiles_for_layer(layout, TileLayers.LayerType.TARGET, raw_tiles.Targets)
+	add_tiles_for_layer(layout, TileLayers.LayerType.BOX, raw_tiles.Boxes)
+	add_tiles_for_layer(layout, TileLayers.LayerType.TARGET_BOX, raw_tiles.TargetBoxes)
+	layout.set_player_start(ps.x, ps.y)
+	
+	return layout
+
+func load_level_data() -> void:
+	var file = FileAccess.open(LEVEL_DATA_PATH, FileAccess.READ)
+	var raw_data = JSON.parse_string(file.get_as_text())
+	
+	for lns in raw_data.keys():
+		_level_data[lns] = setup_level(lns, raw_data[lns])
---
+++ classes/tile_layers.gd
@@ -0,0 +1,20 @@
+class_name TileLayers
+
+enum LayerType {FLOOR, WALL, TARGET, BOX, TARGET_BOX}
+
+var _floor: Array[Vector2i]
+var _wall: Array[Vector2i]
+var _target: Array[Vector2i]
+var _box: Array[Vector2i]
+var _target_box: Array[Vector2i]
+
+var _layer_coords: Dictionary = {
+	LayerType.FLOOR: _floor,
+	LayerType.WALL: _wall,
+	LayerType.TARGET: _target,
+	LayerType.BOX: _box,
+	LayerType.TARGET_BOX: _target_box,
+}
+
+func add_coord(coord: Vector2i, type: LayerType) -> void:
+	_layer_coords[type].push_back(coord)
